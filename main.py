5153b4777608b52265131b4c9c59c8a92d7c8fb2
Jonas <jonasmindaugasr@gmail.com>
2024-09-30 07:15:13 +0300

    UPD: added iteration over signal lengths.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 import functions as f
 
 file_pth = r'H:\magistro_studijos\magis\data_eyes'
-# intervals = [[500, 1000], [750, 1250], [1000, 1500]]
+lengths_list = [300, 500, 1000]
 connectivity_features_fun = [f.pli_features, f.plv_features, f.imag_part_coh_features, f.coherence_features, f.adj_features, f.svd_features]
 
 # if __name__ == '__main__':
@@ -19,17 +19,14 @@ connectivity_features_fun = [f.pli_features, f.plv_features, f.imag_part_coh_fea
 if __name__ == '__main__':
     start_time_multi = time.time()
 
-    intervals = []
-    for i in range(1, 11):
-        intervals.append(f.generate_intervals(0, 9000, i*100))
+    for length in lengths_list:
+        intervals = f.generate_intervals(0, 9000, 500)
 
-    intervals = [i for sublist in intervals for i in sublist]
-
-    graph_summary_df = f.summarize_graphs_in_parallel_2(file_pth=f'{file_pth}/eeg_eyes_numpy_arrays_2.npy',
-                                                        intervals=intervals,
-                                                        connectivity_features_fun=connectivity_features_fun)
-    end_time_multi = time.time()
-    print(end_time_multi - start_time_multi)
-    print(graph_summary_df.head())
-    graph_summary_df.to_csv(f'{file_pth}/non_overlapping_df.csv')
+        graph_summary_df = f.summarize_graphs_in_parallel_2(file_pth=f'{file_pth}/eeg_eyes_numpy_arrays_2.npy',
+                                                            intervals=intervals,
+                                                            connectivity_features_fun=connectivity_features_fun)
+        end_time_multi = time.time()
+        print(end_time_multi - start_time_multi)
+        print(graph_summary_df.head())
+        graph_summary_df.to_csv(f'{file_pth}/non_overlapping_{length}_df.csv')
 
